99d7c993f779c6086d05a035a24ec78c52e4c848
panwoyski <jonak.t.o@gmail.com>
2017-07-28 14:06:26 +0200

    Dunno, some work done

--- implementations/biclustering/cheng_church.py
+++ implementations/biclustering/cheng_church.py
@@ -1,36 +1,32 @@
 import numpy as np
 
 
-class MeanCachedMatrix:
-    def __init__(self, nd_array):
-        self.__array = nd_array
+def calculate_means(matrix):
+    row_means = np.sum(matrix, axis=1) / matrix.shape[1]
+    col_means = np.sum(matrix, axis=0) / matrix.shape[0]
+    matrix_mean = np.sum(row_means) / matrix.shape[0]
+    return matrix_mean, row_means, col_means
 
-        self.rows_count, self.cols_count = self.__array.shape
 
-        row_means = np.sum(nd_array, axis=1) / self.cols_count
-        self.__row_means = {index: mean for index, mean in enumerate(row_means)}
-        col_means = np.sum(nd_array, axis=0) / self.rows_count
-        self.__column_means = {index: mean for index, mean in enumerate(col_means)}
-        self._matrix_mean = sum(self.__row_means.values()) / self.rows_count
+class BiclusteringProblem:
+    def __init__(self, matrix):
+        self._matrix = matrix
+        self.I = list(range(matrix.shape[0]))
+        self.J = list(range(matrix.shape[1]))
 
-    def erase_row(self, row_index):
-        self.__column_means = np.subtract(self.__column_means, (self.__array[row_index, :] / self.cols_count))
-        dropped_row_mean = self.__row_means[row_index]
-        old_rows_count = self.rows_count
+    def _calculate_i_mean(self, i):
+        return np.sum(self._matrix[i, self.J]) / self._matrix.shape[0]
 
-        self.rows_count -= 1
-        self.__row_means.pop(row_index)
+    def _calculate_j_mean(self, j):
+        return np.sum(self._matrix[self.I, j]) / self._matrix.shape[1]
 
-        self._matrix_mean = ((self._matrix_mean - (dropped_row_mean / old_rows_count)) * old_rows_count / self.rows_count)
+    def _calculate_matrix_mean(self):
+        return np.sum(self._matrix) / np.multiply(*self._matrix.shape)
 
-    def erase_column(self, column_index):
-        self.__row_means -= self.__array[:, column_index] / self.cols_count
-        dropped_col_mean = self.__column_means[column_index]
-        old_cols_count = self.cols_count
+    def _calculate_di(self, i):
 
-        self.cols_count -= 1
-        self.__column_means.pop(column_index)
+        return
 
-        self._matrix_mean = ((self._matrix_mean - (dropped_col_mean / old_cols_count)) * old_cols_count / self.cols_count)
+    def _calculate_dj(self, j):
 
-    # def add_column(self, column_index):
+    def solve(self):
